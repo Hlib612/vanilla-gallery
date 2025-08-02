9b8d31f886a4e0a60f940f334d9f7ac1e22d846d
Hlib612 <glebmalonos@gmail.com>
2025-08-02 11:30:01 +0300

    add some colorPickerAction

---
+++ src/js/color_picker.js
@@ -0,0 +1,53 @@
+
+
+const colorPallete = document.querySelector('.color-palette');
+// console.log(colorPallete);
+const output = document.querySelector('.output');
+// console.log(output);
+
+const createPalletItems = function(){
+    const array = [];
+for( let i = 0; i < 36; i += 1){
+    const button = document.createElement('button');
+    const buttonColor = getRandomHexColor();
+    button.style.backgroundColor = buttonColor;
+    button.classList.add('item');
+    button.dataset.color = buttonColor;
+    array.push(button);
+    // console.log(button);
+}
+colorPallete.append(...array);
+}
+createPalletItems()
+
+const colorPickerAction = event => {
+    if(event.target.nodeName !== 'BUTTON'){
+        return ;
+    }
+    // console.log(event.target)
+    // console.log(event.target.nodeName)
+    // console.log(event.target.dataset.color)
+    const color = event.target.dataset.color;
+    output.textContent = `Selected color: - ${color}`;
+    output.style.color = color;
+}
+
+
+colorPallete.addEventListener('click' , colorPickerAction);
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+  
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
